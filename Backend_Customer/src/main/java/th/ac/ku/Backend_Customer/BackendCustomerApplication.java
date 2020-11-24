eb4d86ca96841e69af74a5b4886227ddd1a355ae
earnvinz <erb.u@ku.th>
2020-11-24 11:02:40 +0700

    Jmeter CSV and jmx

--- Backend_Customer/src/main/java/th/ac/ku/Backend_Customer/BackendCustomerApplication.java
+++ Backend_Customer/src/main/java/th/ac/ku/Backend_Customer/BackendCustomerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendCustomerApplication {
+public class BackendCustomerApplication { // 8090
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendCustomerApplication.class, args);
